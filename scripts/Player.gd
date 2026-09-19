89904ab7e5826cd35c28703579e9b202d6d0ceb7
SudipDas <sudip.game.dev@gmail.com>
2026-09-19 11:52:19 +0530

    screwed the cam physics

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,61 +1,71 @@
 extends RigidBody3D
 
-@export var MOV_MODE := true # 'true' for WASD and 'false' for DRAGnSHOOT.
+@export var mov_mode := true # 'true' for WASD, 'false' for DRAGnSHOOT
 @export var roll_torque: float = 2.0
+@export var smooth_speed: float = 16.0
 
-var input_dir: Vector2
-var torque_vector : Vector3
+@onready var camera_node: Camera3D = get_viewport().get_camera_3d()
 
-# Called when the node enters the scene tree for the first time.
+var input_dir: Vector2 = Vector2.ZERO
+var torque_vector: Vector3 = Vector3.ZERO
+
 func _ready() -> void:
-	$CameraRayCast.add_exception(self) # Add self as exception for the CameraRayCast
+	# Ignore self so the camera's raycast doesn't hit the player mesh
+	$CameraRayCast.add_exception(self)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-		DebugDraw3D.draw_arrow(%CollisionShape.global_position, %CollisionShape.global_position + %CollisionShape.get_parent().linear_velocity * 0.3, Color.BLUE, 0.01)
-
-
+func _process(_delta: float) -> void:
+	# Draw velocity helper arrow
+	var arrow_start = %CollisionShape.global_position
+	var arrow_end = arrow_start + linear_velocity * 0.3
+	DebugDraw3D.draw_arrow(arrow_start, arrow_end, Color.BLUE, 0.01)
 
 func _input(event: InputEvent) -> void:
-	#Toggling MOV_MODE
+	# Toggle Movement Mode
 	if event.is_action_pressed("change_mov_mode"):
-		MOV_MODE = not MOV_MODE
-		print("MOV_MODE changed to:" + str(MOV_MODE) + ".")
+		mov_mode = not mov_mode
+		print("MOV_MODE changed to: ", mov_mode)
 		
-	#Exiting the scene [for development stage]
+	# Developer shortcut to close the active testing instance
 	if event.is_action_pressed("exit"):
+		print("QUIT ( ", get_tree().current_scene.name, " ) SCENE!")
 		get_tree().quit()
-		print("QUIT ( " + str(get_tree().current_scene.name) + " )SCENE!")
-	
 
 func _physics_process(delta: float) -> void:
-	#MOVEMENT
-	player_mov(MOV_MODE)
-
-	
-func player_mov(MOV_MODE):
-	#Setting Movement Modes as per MOV_MODE
-	if MOV_MODE == true:
-		wasd_mov()
+	# Divert control pipelines based on current toggle state
+	if mov_mode:
+		wasd_mov(delta)
 	else:
 		dragnshoot_mov()
-		
 
-func wasd_mov():
-	#print("WASD MOVEMENT MODE ACTIVATED")
-	input_dir = input_dir.lerp(Input.get_vector("move_left", "move_right", "move_forward", "move_backward"), 16.0 * get_physics_process_delta_time())
+func wasd_mov(delta: float) -> void:
+	# 1. Gather smoothly interpolated input axis vectors
+	var raw_input = Input.get_vector("move_left", "move_right", "move_backward", "move_forward")
+	input_dir = input_dir.lerp(raw_input, smooth_speed * delta)
 	
-	# Debugdraw3D
-	DebugDraw2D.set_text("INPUT_DIR", input_dir)
-	DebugDraw2D.set_text("TORQUE VECTOR :", torque_vector)
-	DebugDraw3D.draw_arrow_ray(self.global_position, Vector3(input_dir.x, 0.0, input_dir.y), 1.0, Color.BLUE, 0.1)
+	# 2. Extract camera basis directions flattened onto the floor horizontal ground plane
+	var cam_forward = -camera_node.global_transform.basis.z
+	var cam_right = camera_node.global_transform.basis.x
 	
-	#Roll as per input_dir
-	torque_vector = Vector3(input_dir.y, 0, -input_dir.x) * roll_torque
-	torque_vector = torque_vector.lerp(Vector3(input_dir.y, 0, -input_dir.x) * roll_torque, 16.0 * get_tree().root.get_physics_process_delta_time())
+	cam_forward.y = 0.0
+	cam_right.y = 0.0
+	cam_forward = cam_forward.normalized()
+	cam_right = cam_right.normalized()
+	
+	# 3. Translate 2D input space directly into the camera-facing 3D movement path
+	var move_direction = (cam_right * input_dir.x) + (cam_forward * input_dir.y)
+	
+	# 4. Convert structural move direction vector into true rolling physics forces (Perpendicular Torque)
+	# Moving "forward" means spinning around the horizontal axis perpendicular to forward direction
+	var target_torque = Vector3(move_direction.z, 0.0, -move_direction.x) * roll_torque
+	torque_vector = torque_vector.lerp(target_torque, smooth_speed * delta)
+	
+	# Apply final physics rotational force impulse
 	apply_torque(torque_vector)
 	
-func dragnshoot_mov():
-	#print("DRAGnSHOOT MOVEMENT MODE ACTIVATED")
+	# Debug Telemetry Outputs
+	DebugDraw2D.set_text("INPUT_DIR", input_dir)
+	DebugDraw2D.set_text("TORQUE VECTOR :", torque_vector)
+	DebugDraw3D.draw_arrow_ray(global_position, move_direction, 1.0, Color.BLUE, 0.1)
+
+func dragnshoot_mov() -> void:
 	pass
--- scripts/PlayerCamera.gd
+++ scripts/PlayerCamera.gd
@@ -1,106 +1,82 @@
 extends Node3D
 
-# Mouse Config [look]
+# Mouse Config
 @export var mouse_sensitivity := 0.003
-@export var min_pitch := -80.0 # Don't look straight up
-@export var max_pitch := 80.0  # Don't look straight down
+@export var min_pitch := -80.0
+@export var max_pitch := 80.0
+@export var camera_look_smooth_speed := 15.0
 
-@onready var CAMERA := %Camera3D
-@onready var CAMERA_ANCHOR := get_node("../PlayerMain/CameraAnchor")
-@onready var CAMERA_RAYCAST := get_node("../PlayerMain/CameraRayCast")
-@onready var PLAYER := get_node("../PlayerMain/Mesh")
-
+# Camera Parameters
 @export var camera_range := 1.0
+@export var camera_offset : float = 4.0 # Acts as your default Spring Arm Length
 @export var fov_fac := 12.0
 
-@onready var camera_anchor_offset
-@export var camera_offset : float = 4.0 # Acts as your sphere's radius
-#@export var camera_offset : Vector3 = Vector3(0.0, 3.0, 3.0)
-@onready var player_pos : Vector3 = PLAYER.global_position
+# Node References
+@onready var SPRING_ARM: SpringArm3D = $SpringArm3D
+@onready var CAMERA := %Camera3D
+@onready var CAMERA_ANCHOR := get_node("../PlayerMain/CameraAnchor")
+@onready var PLAYER := get_node("../PlayerMain/Mesh")
 
+# State Tracking Variables
+var camera_anchor_offset: float = 0.0
+var player_pos : Vector3 = Vector3.ZERO
 var prev_pos : Vector3 = Vector3.ZERO
 var camera_anchor_pos : Vector3 = Vector3.ZERO
 
-#looking cam rotations
-var rot_x : float = 0.0 # Left/Right rotation (Yaw)
-var rot_y : float = 0.0 # Up/Down rotation (Pitch)
+# Look Rotations (Target vs Smoothed Values)
+var target_rot_x : float = 0.0
+var target_rot_y : float = 0.0
+var smoothed_rot_x : float = 0.0
+var smoothed_rot_y : float = 0.0
 
-
-
-#var space_state := get_world_3d().direct_space_state
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	camera_anchor_pos = CAMERA_ANCHOR.global_position
-	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED #for now...
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED 
 	
-	
-	
+	# Apply your camera_offset export variable straight to the spring arm length
+	SPRING_ARM.spring_length = camera_offset
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rot_x -= event.relative.x * mouse_sensitivity
-		rot_y -= event.relative.y * mouse_sensitivity
-		
-		#clamp it
-		rot_y = clamp(rot_y, deg_to_rad(min_pitch), deg_to_rad(max_pitch))
-		
-	
-	
+		target_rot_x -= event.relative.x * mouse_sensitivity
+		target_rot_y -= event.relative.y * mouse_sensitivity
+		target_rot_y = clamp(target_rot_y, deg_to_rad(min_pitch), deg_to_rad(max_pitch))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#Camera Anchor Offset Distance
+	var proc_delta = get_process_delta_time()
+	
+	# 1. Smoothly follow the player's position anchor
 	camera_anchor_offset = CAMERA_ANCHOR.global_position.distance_to(camera_anchor_pos)
-	# Smooth Camera Anchor Position
-	camera_anchor_pos = camera_anchor_pos.lerp( CAMERA_ANCHOR.global_position , 8.0 * get_physics_process_delta_time())
-	# Update Player Position
+	camera_anchor_pos = camera_anchor_pos.lerp(CAMERA_ANCHOR.global_position, 8.0 * proc_delta)
+	global_position = camera_anchor_pos
 	player_pos = PLAYER.global_position
 	
-	#project camera onto sphere
-	var direction := Vector3.ZERO
-	direction.x = sin(rot_x) * cos(rot_y)
-	direction.y = sin(rot_y)
-	direction.z = cos(rot_x) * cos(rot_y)
+	# 2. Smoothly lerp looking angle values
+	smoothed_rot_x = lerp(smoothed_rot_x, target_rot_x, camera_look_smooth_speed * proc_delta)
+	smoothed_rot_y = lerp(smoothed_rot_y, target_rot_y, camera_look_smooth_speed * proc_delta)
 	
-	CAMERA.global_position = camera_anchor_pos + (direction * camera_offset)
-
-	CAMERA.look_at(camera_anchor_pos, Vector3.UP)
+	# 3. Rotate the SpringArm3D node (Physics engine moves the child Camera3D instantly)
+	SPRING_ARM.rotation.x = smoothed_rot_y
+	SPRING_ARM.rotation.y = smoothed_rot_x
+	
+	
 
 func _physics_process(delta: float) -> void:
-	
-	
-	# Placing the camera inside a range
 	camera_ranging(camera_range)
-	# Look at the player
-	#CAMERA.look_at(player_pos, Vector3.UP)
-	# Camera FOV effect
 	camera_fov(delta)
-	#print(camera_anchor_pos)
 
-
-func camera_ranging(radius):
-	# DebugDraw3D
-	DebugDraw3D.draw_gizmo(Transform3D(Basis(), player_pos), Color.BLUE, true)
+func camera_ranging(radius: float) -> void:
+	# Debug visualization telemetry readouts
+	DebugDraw3D.draw_position(Transform3D(Basis(), camera_anchor_pos), Color.GREEN)
+	
 	DebugDraw2D.set_text("CAMERA_ANCHOR_POS : ", camera_anchor_pos)
 	DebugDraw2D.set_text("CAMERA_ANCHOR_OFFSET : ", camera_anchor_offset)
 	DebugDraw2D.set_text("CAMERA_FOV : ", CAMERA.fov)
 	DebugDraw2D.set_text("FOV_FAC : ", fov_fac)
-	DebugDraw3D.draw_position(Transform3D(Basis(), camera_anchor_pos), Color.GREEN)
-	DebugDraw3D.draw_arrow(camera_anchor_pos, CAMERA_RAYCAST.get_collision_point(), Color.RED, 0.05)
-	
-	#Simpler Method
-	#CAMERA.global_position = camera_anchor_pos + camera_offset
-	
-	# Set CameraRaycast Position to CameraAnchor
-	CAMERA_RAYCAST.global_position = camera_anchor_pos
-	
-	if CAMERA_RAYCAST.is_colliding():
-		DebugDraw2D.set_text("Raycast Hit At", CAMERA_RAYCAST.get_collision_point())
-		DebugDraw3D.draw_position(Transform3D(Basis(), CAMERA_RAYCAST.get_collision_point()), Color.RED)
-	else:
-		DebugDraw2D.set_text("Raycast not hit.")
+	var is_colliding = SPRING_ARM.get_hit_length() < SPRING_ARM.spring_length
+	DebugDraw2D.set_text("SpringArm Collision Active: ", is_colliding)
 
-
-func camera_fov(delta):
-	CAMERA.fov = lerp(CAMERA.fov, 75.0 + ( (camera_anchor_pos.distance_to(prev_pos) / delta) * fov_fac ), 0.1)
+func camera_fov(delta: float) -> void:
+	var current_speed = camera_anchor_pos.distance_to(prev_pos) / delta
+	CAMERA.fov = lerp(CAMERA.fov, 75.0 + (current_speed * fov_fac), 0.1)
 	prev_pos = camera_anchor_pos
